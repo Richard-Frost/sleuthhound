2c14ad3413ad84a18e13192f524d3008fcab98d3
Richard-Frost <frost.rchrd.com>
2022-07-08 13:18:22 -0700

    agency backend working

--- sleuthhound_api/config/routes.rb
+++ sleuthhound_api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :clients
   resources :animals
   resources :users
   get '/pets'=> 'get_pets_services#index'
   post '/pets'=> 'get_pets_services#create'
-  
+  post '/agency' => 'users#agency'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
